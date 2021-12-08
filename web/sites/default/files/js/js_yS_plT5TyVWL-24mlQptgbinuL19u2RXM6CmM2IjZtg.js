6bd287cd76bdb04015ca84285294d94c3c37959d
waqar7835 <waqar7835@gmail.com>
2021-12-08 12:27:28 +0000

    configuration export

---
+++ web/sites/default/files/js/js_yS_plT5TyVWL-24mlQptgbinuL19u2RXM6CmM2IjZtg.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.leatherista = {
+    attach: function (context, settings) {
+      
+    }
+  };
+
+})(jQuery, Drupal);
+;
