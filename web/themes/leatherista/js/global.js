b6d2292b751a29a20014566c3061ecba73f60ddc
Syed Naveed <navedzulfiquar@gmail.com>
2021-12-09 11:57:31 +0500

    first slider js updated

--- web/themes/leatherista/js/global.js
+++ web/themes/leatherista/js/global.js
@@ -10,6 +10,17 @@
   Drupal.behaviors.leatherista = {
     attach: function (context, settings) {
       
+
+      // top-pick-for-you-slider start 
+      $(".top-pick-for-you-slider").slick({
+        dots: true,
+        vertical: true,
+        centerMode: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+      });
+      // top-pick-for-you-slider end  
+
     }
   };
 
