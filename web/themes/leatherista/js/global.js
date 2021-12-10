4b68df8c11aa13bf551314a1d802ff0f19aac838
Syed Naveed <navedzulfiquar@gmail.com>
2021-12-10 11:26:25 +0500

    js path set

--- web/themes/leatherista/js/global.js
+++ web/themes/leatherista/js/global.js
@@ -74,6 +74,68 @@
       });
       // top-pick-for-you-slider end  
 
+      // testimonials-front-slider start 
+      $(".testimonials-front-slider").slick({
+        infinite: true,
+        draggable: true,
+        fade: false,
+        swipe: true,
+        slidesToShow: 1,
+        autoplay: false,
+        speed: 1000,
+        autoplaySpeed: 15000,
+        slidesToScroll: 1,
+        rows: 1,
+        pauseOnHover: false,
+        dots: false,
+        arrows: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              
+              autoplay: true,
+
+          }
+          },
+          {
+            breakpoint: 769,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              
+              // autoplay: false,
+              arrows: false,
+            }
+          },
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+               
+              // autoplay: false,
+              arrows: false,
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows: false,
+              
+            }
+          }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+      });
+      // testimonials-front-slider end 
+
     }
   };
 
