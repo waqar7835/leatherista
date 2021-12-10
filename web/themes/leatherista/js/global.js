e2bf7e1594ec51a7d27996b06a8c903c61f59ff9
Syed Naveed <navedzulfiquar@gmail.com>
2021-12-10 11:00:57 +0500

    js path set

--- web/themes/leatherista/js/global.js
+++ web/themes/leatherista/js/global.js
@@ -13,11 +13,38 @@
 
       // top-pick-for-you-slider start 
       $(".top-pick-for-you-slider").slick({
-        dots: true,
-        vertical: true,
-        centerMode: true,
+        dots: false,
+        infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
       });
       // top-pick-for-you-slider end  
 
