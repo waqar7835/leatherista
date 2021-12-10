1ffbb1e96d7ea560bd2736ef9bc32899c24134cc
Syed Naveed <navedzulfiquar@gmail.com>
2021-12-10 11:11:34 +0500

    js path set

--- web/themes/leatherista/js/global.js
+++ web/themes/leatherista/js/global.js
@@ -17,7 +17,7 @@
         draggable: true,
         swipe: true,
         centerPadding: '265px',
-        slidesToShow: 1,
+        slidesToShow: 3,
         autoplay: false,
         speed: 1000,
         autoplaySpeed: 15000,
@@ -25,6 +25,52 @@
         rows: 1,
         pauseOnHover: false,
         dots: false,
+        prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              
+              autoplay: true,
+
+          }
+          },
+          {
+            breakpoint: 769,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              
+              // autoplay: false,
+              arrows: false,
+            }
+          },
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+               
+              // autoplay: false,
+              arrows: false,
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows: false,
+              
+            }
+          }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
       });
       // top-pick-for-you-slider end  
 
