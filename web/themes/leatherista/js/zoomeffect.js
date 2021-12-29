00a7c790571b041e75b5e73b7d2ef02c11349070
atta-ur-rehman6685 <attaurrehman6685@gmail.com>
2021-12-29 19:02:57 +0500

    custom file zoomeffect ùpdated

--- web/themes/leatherista/js/zoomeffect.js
+++ web/themes/leatherista/js/zoomeffect.js
@@ -1,14 +1,7 @@
-// Example:
 jQuery(document).ready(function(){
-    jQuery('a.photo').zoom({url: 'photo-big.jpg'});
+    jQuery('.path-product.field--name-field-images img')
+      .wrap('<span style="display:inline-block"></span>')
+      .css('display', 'block')
+      .parent()
+      .zoom();
   });
-  
-  // Using Colorbox with Zoom
-//   $(document).ready(function(){
-//     $('a.photo').zoom({
-//       url: 'photo-big.jpg', 
-//       callback: function(){
-//         $(this).colorbox({href: this.src});
-//       }
-//     });
-//   });
