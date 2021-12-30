950d8c09198ae61da1cd5fede950c3a9e5d1e811
atta-ur-rehman6685 <attaurrehman6685@gmail.com>
2021-12-30 11:23:37 +0500

    custom file zoomeffect ùpdated

--- web/themes/leatherista/js/zoomeffect.js
+++ web/themes/leatherista/js/zoomeffect.js
@@ -2,6 +2,5 @@ jQuery(document).ready(function(){
     jQuery('.path-product .field--name-field-images img')
       .wrap('<span style="display:inline-block"></span>')
       .css('display', 'block')
-      .parent()
       .zoom({magnify:1});
   });
