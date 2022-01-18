08bbf71533988b47f70dcbe2911a796e33b3c14c
Atta-ur-rehman <personal@Atta-ur-rehmans-MacBook-Pro.local>
2022-01-18 10:53:07 +0500

    global.js updated

--- web/themes/leatherista/js/global.js
+++ web/themes/leatherista/js/global.js
@@ -10,9 +10,6 @@
   Drupal.behaviors.leatherista = {
     attach: function (context, settings) {
       
-      //cart popup block 
-      $("#block-commerce-popup-cart-commerce-popup-cart #commerce-cart-pop-block .remove-button input.delete-order-item").prop('value', 'x');
-      
       // top-pick-for-you-slider start 
       $(".top-pick-for-you-slider").slick({
         infinite: true,
